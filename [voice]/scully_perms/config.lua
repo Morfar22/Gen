ba30fbc1fdff317c63d8f940dc71fc023bf9810f
Morfar <47919859+7R3C3@users.noreply.github.com>
2022-08-15 10:45:43 +0200

    Fjernet discord perms
    Fjernet discord perms

--- [voice]/scully_perms/config.lua
+++
@@ -1,9 +0,0 @@
-Scully = {
-    Debug = false, -- This is for Mr. Scully :)
-    Guild = '962835408016781364', -- Your discord guild ID. (Tutorial: https://github-wiki-see.page/m/manix84/discord_gmod_addon_v2/wiki/Finding-your-Guild-ID-%28Server-ID%29)
-    Token = 'MTAwMDA2MTYwNDI1MzE1OTQ3NQ.GnA_zW.55qc5Y1MalEiSyaQK-WL2Xj9Ekit0X41bNAiVQ', -- Your discord bot token from the discord developer portal. (Tutorial: https://www.writebots.com/discord-bot-token/)
-    Permissions = { -- Your permissions list, can be used as a string or table.
-        ['police'] = '964251128197955676', -- Method example 1
-        ['ems'] = '964251128197955676', -- Method example 2
-    }
-}
