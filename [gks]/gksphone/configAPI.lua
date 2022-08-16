14693481859d034a2bf9e04c45f51b6cdf1384b7
Morfar <47919859+7R3C3@users.noreply.github.com>
2022-08-16 18:40:57 +0200

    Tilføjet ny telefon, nyt job og ændret priser i shoppen

---
+++ [gks]/gksphone/configAPI.lua
@@ -0,0 +1,27 @@
+Cfg = {}
+
+-- Discord Webhook - start --
+
+Cfg.MessagesWebhook = false
+Cfg.CallWebhooks    = false
+
+Cfg.VoiceMail       = "DISCORD WEBHOOK"
+Cfg.PhotoWebHook    = "DISCORD WEBHOOK"
+Cfg.Carseller       = "DISCORD WEBHOOK"
+Cfg.NewCarSeller    = "DISCORD WEBHOOK"
+Cfg.JobNotif        = "DISCORD WEBHOOK"
+Cfg.TwitterWeb      = "DISCORD WEBHOOK"
+Cfg.YellowWeb       = "DISCORD WEBHOOK"
+Cfg.InstagramWeb    = "DISCORD WEBHOOK"
+Cfg.Crypto          = "DISCORD WEBHOOK"
+Cfg.BankTransfer    = "DISCORD WEBHOOK"
+Cfg.RaceWinning     = "DISCORD WEBHOOK"
+Cfg.RaceCreate      = "DISCORD WEBHOOK"
+Cfg.MessageWeb      = "DISCORD WEBHOOK"
+Cfg.CallWebhook     = "DISCORD WEBHOOK"
+Cfg.BankLimit       = 5000 -- # Minimum money transfer for Discord Webhook
+Cfg.ExploitWebhook  = "DISCORD WEBHOOK"
+
+-- Discord Webhook - end --
+
+
