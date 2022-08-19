6acfe3014c25bf4c701abc0bb35c3c481d663393
Morfar <47919859+7R3C3@users.noreply.github.com>
2022-08-19 22:13:47 +0200

    Oversat qb-vehiclesale

--- [qb]/qb-vehiclesales/locales/en.lua
+++ [qb]/qb-vehiclesales/locales/en.lua
@@ -1,38 +1,38 @@
 local Translations = {
     error = {
-        not_your_vehicle = 'This is not your vehicle..',
-        vehicle_does_not_exist = 'Vehicle does not exist',
-        not_enough_money = 'You dont have enough money',
-        finish_payments = 'You must finish paying off this vehicle, Before you can sell it..',
-        no_space_on_lot = 'There is not space for your car on the lot!'
+        not_your_vehicle = 'Dette er ikke dit køretøj..',
+        vehicle_does_not_exist = 'Køretøjet findes ikke',
+        not_enough_money = 'Du har ikke penge nok',
+        finish_payments = 'Du skal afslutte med at betale af på dette køretøj, før du kan sælge det..',
+        no_space_on_lot = 'Der er ikke plads til din bil på grunden!'
     },
     success = {
-        sold_car_for_price = 'You have sold your car for $%{value}',
-        car_up_for_sale = 'Your car has been put up for sale! Price - $%{value}',
-        vehicle_bought = 'Vehicle Bought'
+        sold_car_for_price = 'Du har solgt din bil for $%{value}',
+        car_up_for_sale = 'Din bil er sat til salg! Pris - $%{value}',
+        vehicle_bought = 'Købt køretøj'
     },
     info = {
-        confirm_cancel = '~g~Y~w~ - Confirm / ~r~N~w~ - Cancel ~g~',
-        vehicle_returned = 'You vehicle is returned',
-        used_vehicle_lot = 'Used Car Lot',
-        sell_vehicle_to_dealer = '[~g~E~w~] - Sell Vehicle To Dealer For ~g~$%{value}',
-        view_contract = '[~g~E~w~] - View Vehicle Contract',
-        cancel_sale = '[~r~G~w~] - Cancel Vehicle Sale',
-        model_price = '%{value}, Price: ~g~$%{value2}',
-        are_you_sure = 'Are you sure you no longer want to sell your vehicle?',
-        yes_no = '[~g~7~w~] - Yes | [~r~8~w~] - No',
-        place_vehicle_for_sale = '[~g~E~w~] - Place Vehicle For Sale By Owner'
+        confirm_cancel = '~g~Y~w~ - Bekræft / ~r~N~w~ - Annuller ~g~',
+        vehicle_returned = 'Dit køretøj er returneret',
+        used_vehicle_lot = 'Brugt bilparti',
+        sell_vehicle_to_dealer = '[~g~E~w~] - Sælg køretøj til forhandler for ~g~$%{value}',
+        view_contract = '[~g~E~w~] - Se køretøjskontrakt',
+        cancel_sale = '[~r~G~w~] - Annuller bilsalg',
+        model_price = '%{value}, pris: ~g~$%{value2}',
+        are_you_sure = 'Er du sikker på, at du ikke længere vil sælge dit køretøj?',
+        yes_no = '[~g~7~w~] - Ja | [~r~8~w~] - Nej',
+        place_vehicle_for_sale = '[~g~E~w~] - Placer køretøj til salg af ejer'
     },
     charinfo = {
-        firstname = 'not',
-        lastname = 'known',
-        account = 'Account not known..',
-        phone = 'telephone number not known..'
+        fornavn = 'ikke',
+        efternavn = 'kendt',
+        account = 'Konto ikke kendt..',
+        phone = 'telefonnummer ikke kendt..'
     },
     mail = {
-        sender = 'Larrys RV Sales',
-        subject = 'You have sold a vehicle!',
-        message = 'You made $%{value} from the sale of your %{value2}.'
+        afsender = 'Larrys RV Salg',
+        subject = 'Du har solgt et køretøj!',
+        message = 'Du tjente $%{value} på salget af din %{value2}.'
     }
 }
 
