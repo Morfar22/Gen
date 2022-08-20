65798df4878387720b3ebccc503f9ce01e1aea16
Morfar <47919859+7R3C3@users.noreply.github.com>
2022-08-20 15:21:09 +0200

    Nyt inventory og fikset multikara

--- [qb]/qb-multicharacter/html/script.js
+++ [qb]/qb-multicharacter/html/script.js
@@ -31,7 +31,7 @@ $(document).ready(function (){
                 $(".welcomescreen").fadeIn(150);
                 qbMultiCharacters.resetAll();
 
-                var originalText = "Loader dine karaktere";
+                var originalText = "Retrieving player data";
                 var loadingProgress = 0;
                 var loadingDots = 0;
                 $("#loading-text").html(originalText);
@@ -42,15 +42,15 @@ $(document).ready(function (){
                     loadingDots++;
                     loadingProgress++;
                     if (loadingProgress == 3) {
-                        originalText = "Valider spiller data"
+                        originalText = "Validating player data"
                         $("#loading-text").html(originalText);
                     }
                     if (loadingProgress == 4) {
-                        originalText = "Henter karaktere"
+                        originalText = "Retrieving characters"
                         $("#loading-text").html(originalText);
                     }
                     if (loadingProgress == 6) {
-                        originalText = "Valider spiller"
+                        originalText = "Validating characters"
                         $("#loading-text").html(originalText);
                     }
                     if(loadingDots == 4) {
@@ -65,7 +65,7 @@ $(document).ready(function (){
                     setTimeout(function(){
                         clearInterval(DotsInterval);
                         loadingProgress = 0;
-                        originalText = "Modtager data";
+                        originalText = "Retrieving data";
                         $(".welcomescreen").fadeOut(2000);
                         $('.imagenlogo').addClass('entrada');
                         $(".title-screen").fadeIn(100);
@@ -140,20 +140,20 @@ $(".btn-iniciar").on("click", function() {
 
 function setupCharInfo(cData) {
     if (cData == 'empty') {
-        $('.character-info-valid').html('<span id="no-char">Den valgte karakterplads er ikke i brug endnu.<br><br>Denne karakter har endnu ingen information.</span>');
+        $('.character-info-valid').html('<span id="no-char">The selected character slot is not in use yet.<br><br>This character doesn\'t have information yet.</span>');
     } else {
         var gender = "Man"
         if (cData.charinfo.gender == 1) { gender = "Woman" }
         $('.character-info-valid').html(
-        '<div class="character-info-box"><span id="info-label">Navn: </span><span class="char-info-js">'+cData.charinfo.firstname+' '+cData.charinfo.lastname+'</span></div>' +
-        '<div class="character-info-box"><span id="info-label">Fødelsdata: </span><span class="char-info-js">'+cData.charinfo.birthdate+'</span></div>' +
-        '<div class="character-info-box"><span id="info-label">Køn: </span><span class="char-info-js">'+gender+'</span></div>' +
-        '<div class="character-info-box"><span id="info-label">Nationalitet: </span><span class="char-info-js">'+cData.charinfo.nationality+'</span></div>' +
+        '<div class="character-info-box"><span id="info-label">Name: </span><span class="char-info-js">'+cData.charinfo.firstname+' '+cData.charinfo.lastname+'</span></div>' +
+        '<div class="character-info-box"><span id="info-label">Birth date: </span><span class="char-info-js">'+cData.charinfo.birthdate+'</span></div>' +
+        '<div class="character-info-box"><span id="info-label">Gender: </span><span class="char-info-js">'+gender+'</span></div>' +
+        '<div class="character-info-box"><span id="info-label">Nationality: </span><span class="char-info-js">'+cData.charinfo.nationality+'</span></div>' +
         '<div class="character-info-box"><span id="info-label">Job: </span><span class="char-info-js">'+cData.job.label+'</span></div>' +
-        '<div class="character-info-box"><span id="info-label">Kontant: </span><span class="char-info-js">&#36; '+cData.money.cash+'</span></div>' +
+        '<div class="character-info-box"><span id="info-label">Cash: </span><span class="char-info-js">&#36; '+cData.money.cash+'</span></div>' +
         '<div class="character-info-box"><span id="info-label">Bank: </span><span class="char-info-js">&#36; '+cData.money.bank+'</span></div>' +
-        '<div class="character-info-box"><span id="info-label">Telefon nummer: </span><span class="char-info-js">'+cData.charinfo.phone+'</span></div>' +
-        '<div class="character-info-box"><span id="info-label">Konto nummer: </span><span class="char-info-js">'+cData.charinfo.account+'</span></div>');
+        '<div class="character-info-box"><span id="info-label">Phone number: </span><span class="char-info-js">'+cData.charinfo.phone+'</span></div>' +
+        '<div class="character-info-box"><span id="info-label">Account number: </span><span class="char-info-js">'+cData.charinfo.account+'</span></div>');
     }
 }
 
@@ -185,7 +185,7 @@ $(document).on('click', '.character', function(e) {
         selectedChar = $(this);
         if ((selectedChar).data('cid') == "") {
             $(selectedChar).addClass("char-selected");
-            setupCharInfo('Tom')
+            setupCharInfo('empty')
             $("#play-text").html("Create");
             $("#play").css({"display":"block"});
             $("#delete").css({"display":"none"});
@@ -195,8 +195,8 @@ $(document).on('click', '.character', function(e) {
         } else {
             $(selectedChar).addClass("char-selected");
             setupCharInfo($(this).data('cData'))
-            $("#play-text").html('<i class="fa fa-sign-in" aria-hidden="true"></i> Spil');
-            $("#delete-text").html('<i class="fa fa-trash" aria-hidden="true"></i> Slet karakter');
+            $("#play-text").html('<i class="fa fa-sign-in" aria-hidden="true"></i> Play');
+            $("#delete-text").html('<i class="fa fa-trash" aria-hidden="true"></i> Delete');
             $("#play").css({"display":"block"});
             if (EnableDeleteButton) {
                 $("#delete").css({"display":"block"});
@@ -210,8 +210,8 @@ $(document).on('click', '.character', function(e) {
         selectedChar = $(this);
         if ((selectedChar).data('cid') == "") {
             $(selectedChar).addClass("char-selected");
-            setupCharInfo('Tom')
-            $("#play-text").html('<i class="fa fa-plus" aria-hidden="true"></i> Registrer');
+            setupCharInfo('empty')
+            $("#play-text").html('<i class="fa fa-plus" aria-hidden="true"></i> Register');
             $("#play").css({"display":"block"});
             $("#delete").css({"display":"none"});
             $.post('https://qb-multicharacter/cDataPed', JSON.stringify({
@@ -220,8 +220,8 @@ $(document).on('click', '.character', function(e) {
         } else {
             $(selectedChar).addClass("char-selected");
             setupCharInfo($(this).data('cData'))
-            $("#play-text").html('<i class="fa fa-sign-in" aria-hidden="true"></i> Spil');
-            $("#delete-text").html('<i class="fa fa-trash" aria-hidden="true"></i> Slet karakter');
+            $("#play-text").html('<i class="fa fa-sign-in" aria-hidden="true"></i> Play');
+            $("#delete-text").html('<i class="fa fa-trash" aria-hidden="true"></i> Delete');
             $("#play").css({"display":"block"});
             if (EnableDeleteButton) {
                 $("#delete").css({"display":"block"});
@@ -314,21 +314,21 @@ $(document).on('click', '#cancel-delete', function(e){
 });
 
 function setCharactersList() {
-    var htmlResult = '<div class="character-list-header"><p>Mine karaktere</p></div>'
+    var htmlResult = '<div class="character-list-header"><p>My Characters</p></div>'
     for (let i = 1; i <= NChar; i++) {
-        htmlResult += '<div class="character" id="char-'+ i +'" data-cid=""><span id="slot-name">Tom plads<span id="cid"></span></span></div>'
+        htmlResult += '<div class="character" id="char-'+ i +'" data-cid=""><span id="slot-name">Empty Slot<span id="cid"></span></span></div>'
     }
-    htmlResult += '<div class="character-btn" id="play"><p id="play-text">Vælg en karakter</p></div><div class="character-btn" id="delete"><p id="delete-text">Vælg en karakter</p></div>'
+    htmlResult += '<div class="character-btn" id="play"><p id="play-text">Select a character</p></div><div class="character-btn" id="delete"><p id="delete-text">Select a character</p></div>'
     $('.characters-list').html(htmlResult)
 }
 
 function refreshCharacters() {
     var htmlResult = ''
     for (let i = 1; i <= NChar; i++) {
-        htmlResult += '<div class="character" id="char-'+ i +'" data-cid=""><span id="slot-name">Tom plads<span id="cid"></span></span></div>'
+        htmlResult += '<div class="character" id="char-'+ i +'" data-cid=""><span id="slot-name">Empty Slot<span id="cid"></span></span></div>'
     }
 
-    htmlResult += '<div class="character-btn" id="play"><p id="play-text">Vælg en karakter</p></div><div class="character-btn" id="delete"><p id="delete-text">Vælg en karakter</p></div>'
+    htmlResult += '<div class="character-btn" id="play"><p id="play-text">Select a character</p></div><div class="character-btn" id="delete"><p id="delete-text">Select a character</p></div>'
     $('.characters-list').html(htmlResult)
     
     setTimeout(function(){
